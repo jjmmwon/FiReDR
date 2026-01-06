b3588288ef4cf0ce340bf9e11f82279f9bdd1b63
jjmmwon <jmw1790@gmail.com>
2026-01-06 16:57:06 +0900

    Refactor aptree types: reorganize project structure

--- prodr/aptree/types/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Sequence
-from numbers import Number
-
-import numpy as np
-import numpy.typing as npt
-
-from .hyperplane import Hyperplane
-from .vector import Vector
---
+++ prodr/types/__init__.py
@@ -0,0 +1,2 @@
+from .vector import Vector
+from .hyperplane import Hyperplane
